fc87c09b984b9348a3c69c692a08c4196108f922
Myron <dubtwee@gmail.com>
2020-03-05 14:02:55 -0600

    initial codebase

---
+++ scripts/client.js
@@ -0,0 +1,11 @@
+console.log('Hello Ursus');
+const basket = [];
+
+// create a function that adds a fruit to my basket
+
+// add some fruit to my basket
+
+// log out basket
+console.log('BASKET:', basket);
+
+// log out every piece of fruit in the basket
